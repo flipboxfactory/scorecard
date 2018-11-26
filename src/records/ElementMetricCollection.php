55ac4bfe0d3b0e389bd26518d66f83921ff9cc64
Nate Iler <nate@flipboxdigital.com>
2018-11-26 12:04:59 -0700

    moving collection metric logic to trait

--- src/records/ElementMetricCollection.php
+++ src/records/ElementMetricCollection.php
@@ -21,131 +21,19 @@ use flipbox\scorecard\metrics\MetricInterface;
  */
 abstract class ElementMetricCollection extends ElementMetric
 {
+     use traits\ElementMetricCollectionTrait;
+
     /**
      * @return array
      */
     const METRICS = [];
 
     /**
-     * @var MetricInterface[]
-     */
-    private $metrics;
-
-
-    /*******************************************
-     * METRIC INTERFACE
-     *******************************************/
-
-    /**
-     * @return float
-     */
-    protected function calculateScore(): float
-    {
-        // Sum of total/weight
-        $total = $weights = 0;
-
-        foreach ($this->getMetrics() as $metric) {
-            $total += $metric->getScore();
-            $weights += $metric->getWeight();
-        }
-
-        return (float)($total / $weights);
-    }
-
-
-    /*******************************************
-     * METRICS (CHILDREN)
-     *******************************************/
-
-    /**
-     * @return MetricInterface[]
-     */
-    public function getMetrics(): array
-    {
-        if ($this->metrics === null) {
-            $this->setMetrics(
-                $this->loadMetrics()
-            );
-        }
-
-        return $this->metrics;
-    }
-
-    /**
      * @return array
      */
-    protected function loadMetrics(): array
+    protected function newMetrics(): array
     {
-        if ($this->getIsNewRecord()) {
-            return (array)static::METRICS;
-        }
-
-        return array_filter(array_merge(
-            $this->children,
-            (array)ArrayHelper::getValue($this->settings, 'metrics', [])
-        ));
-    }
-
-    /**
-     * @param array $metrics
-     * @return $this
-     */
-    public function setMetrics(array $metrics = [])
-    {
-        $this->metrics = [];
-
-        foreach ($metrics as $metric) {
-            if (!$metric instanceof MetricInterface) {
-                $metric = $this->createMetric($metric);
-            }
-
-            $this->metrics[] = $metric;
-        }
-
-        return $this;
-    }
-
-    /**
-     * @noinspection PhpDocMissingThrowsInspection
-     *
-     * @param $metric
-     * @return MetricInterface
-     */
-    protected function createMetric($metric): MetricInterface
-    {
-        if (is_string($metric)) {
-            $metric = ['class' => $metric];
-        }
-
-        /** Pass along the element */
-        if (is_array($metric) && !isset($metric['elementId'])) {
-            $metric['element'] = $metric['element'] ?? $this->getElement();
-        }
-
-        /** @noinspection PhpUnhandledExceptionInspection */
-        /** @noinspection PhpIncompatibleReturnTypeInspection */
-        return MetricHelper::create($metric);
-    }
-
-    /*******************************************
-     * CHILDREN (RECORDS)
-     *******************************************/
-
-    /**
-     * @return ElementMetricQuery
-     */
-    public function getChildren(): ElementMetricQuery
-    {
-        /** @var ElementMetricQuery $query */
-        $query = $this->hasMany(
-            static::class,
-            ['parentId' => 'id']
-        );
-
-        // Children have parents
-        $query->parentId(':notempty:');
-
-        return $query;
+        return (array) static::METRICS;
     }
 
     /*******************************************
@@ -157,16 +45,13 @@ abstract class ElementMetricCollection extends ElementMetric
      */
     public function beforeValidate()
     {
-        $success = true;
-        foreach ($this->getMetrics() as $metric) {
-            if ($metric instanceof ElementMetric) {
-                if (!$metric->validate()) {
-                    $success = false;
-                }
-            }
+        $success = parent::beforeValidate();
+
+        if (!$this->validateMetrics()) {
+            $success = false;
         }
 
-        return $success ? parent::beforeValidate() : false;
+        return $success;
     }
 
     /*******************************************
@@ -178,25 +63,7 @@ abstract class ElementMetricCollection extends ElementMetric
      */
     public function beforeSave($insert)
     {
-        /** @var array $metrics */
-        $metrics = [];
-
-        foreach ($this->getMetrics() as $metric) {
-            if (!$metric instanceof ElementMetric) {
-                $metrics[] = $metric->toConfig();
-            }
-        }
-
-        // Merge into settings
-        if (!empty($metrics)) {
-            $this->settings = array_filter(array_merge(
-                (array)$this->settings,
-                [
-                    'metrics' => $metrics
-                ]
-            ));
-        }
-
+        $this->addMetricsToSettings();
         return parent::beforeSave($insert);
     }
 
@@ -205,30 +72,7 @@ abstract class ElementMetricCollection extends ElementMetric
      */
     public function afterSave($insert, $changedAttributes)
     {
-        /** @var ElementMetric[] $metrics */
-        $children = [];
-
-        foreach ($this->getMetrics() as $metric) {
-            if ($metric instanceof ElementMetric) {
-                $children[] = $metric;
-            }
-        }
-
-        if (!empty($children)) {
-            $success = true;
-            foreach ($children as $child) {
-                $child->parentId = $this->id;
-
-                if (!$child->save()) {
-                    $success = false;
-                }
-            }
-
-            if (!$success) {
-                $this->addError('children', 'Unable to save children');
-            }
-        }
-
+        $this->saveChildRecords();
         return parent::afterSave($insert, $changedAttributes);
     }
 }
